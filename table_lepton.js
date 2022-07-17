2578497492e06d9928482b4cc1a01c6aa3fd5863
Ryosuke AKIZAWA <84809105+Ryoaki374@users.noreply.github.com>
2022-07-17 15:19:16 +0900

    Add files via upload

---
+++ table_lepton.js
@@ -0,0 +1,23 @@
+//ref https://pdg.lbl.gov/2022/tables/rpp2022-sum-baryons.pdf
+
+const lepton = [
+    ["electron", "51099895", "KeV"],
+    ["muon", "1056583755", "MeV"],
+    ["tau", "177686", "MeV"],
+];
+
+const baryon = [
+    ["proton", "93827208816", "MeV"],
+    ["neutron", "9395654205", "MeV"],
+    ["lambda", "1115683", "MeV"],
+    ["sigma", "118937", "MeV"],
+];
+
+const rule = [
+    "rule_restmass","rule_lowercase","rule_uppercase","rule_unit"
+];
+
+const button = [
+    "restmass","uppercase","lowercase","unit"
+]
+
